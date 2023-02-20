2ede05cdd045bf06797f7d12e1b9547244280e27
chstaiger <staigerchristine@gmail.com>
2023-02-20 14:02:57 +0100

    Flake8 check and docstrings

--- experiment/irods.py
+++ experiment/irods.py
@@ -9,7 +9,12 @@ DEFAULT = '\x1b[0m'
 YEL = '\x1b[1;33m'
 BLUE = '\x1b[1;34m'
 
+
 def read_irods_env() -> dict:
+    """
+    Expects a json file in ~/.irods/irods_environment.json
+    Returns the whole file aas dictionary
+    """
     envFile = os.environ['HOME'] + "/.irods/irods_environment.json"
     if os.path.exists(envFile):
         with open(envFile) as f:
@@ -19,43 +24,70 @@ def read_irods_env() -> dict:
         print(RED+"ERROR:", os.environ['HOME']+"/.irods/irods_environment.json not found", DEFAULT)
         sys.exit(1)
 
+
 def test_irods_connection() -> tuple:
+    """
+    Tests whether a connection to an irods server can be established.
+    Expects an irods_environment.json file in ~/.irods.
+    Creates a dictionary from the environment file.
+
+    Returns: irods.session.iRODSSession, dictionary
+    """
     ienv = read_irods_env()
-    res = subprocess.run(["ils"], input="bogus".encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = subprocess.run(["ils"], input="bogus".encode(),
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if res.returncode == 0:
         print("Connected to:", ienv.get('irods_host'))
         print(res.stdout.decode())
-        session = irods.session.iRODSSession(irods_env_file = os.environ['HOME']+"/.irods/irods_environment.json")
+        session = irods.session.iRODSSession(
+                irods_env_file=os.environ['HOME']+"/.irods/irods_environment.json")
         return (session, ienv)
     else:
         print(RED+"ERROR: Cannot connect to iRODS server", DEFAULT)
         print("Please do an iinit")
         sys.exit(1)
 
-def irsync_irods_to_local(irodspath: str, localpath=os.environ['HOME']+"/dump") -> bool:
-    print("DEBUG: Transferring %s --> %s" %(irodspath, localpath))
-    
+
+def irsync_irods_to_local(session: irods.session.iRODSSession, irodspath: str,
+                          localpath=os.environ['HOME']+"/dump") -> bool:
+    """
+    Given an iRODS path and a localpath, transfers data from iRODS to a local filesystem.
+    During the transport checksums are checked on the fly and, if not present, registered in iRODS.
+    Running time can be reduced by firsuring that checksums are already registered in iRODS
+    (running "ichksum irodspath" on commandline).
+
+    Returns: True upon success; False otherwise
+    """
+    print("DEBUG: Transferring %s --> %s" % (irodspath, localpath))
+
     if not os.path.isdir(localpath):
-        print(RED+"ERROR: Destination", localpath, "does not exist", default)
+        print(RED+"ERROR: Destination", localpath, "does not exist", DEFAULT)
         sys.exit(1)
     else:
         itemname = os.path.basename(irodspath)
         if session.collections.exists(irodspath) or session.data_objects.exists(irodspath):
-            res = subprocess.run(["irsync", "-Kr", "i:"+irodspath, localpath+"/"+itemname], 
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            res = subprocess.run(["irsync", "-Kr", "i:"+irodspath, localpath+"/"+itemname],
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if res.returncode == 0:
                 return True
             else:
-                print(RED+"ERROR: Transferring %s --> %s failed" %(irodspath, localpath), DEFAULT)
+                print(RED+"ERROR: Transferring %s --> %s failed" % (irodspath, localpath), DEFAULT)
                 print(res)
                 return False
         else:
-            print(RED+"ERROR: Transferring %s --> %s failed" %(irodspath, localpath), DEFAULT)
+            print(RED+"ERROR: Transferring %s --> %s failed" % (irodspath, localpath), DEFAULT)
             print("\t iRODS path not known.")
             return False
 
 
 def get_irods_size(session: irods.session.iRODSSession, path_names: list) -> int:
+    """
+    Calculates the cumulative file size of a list of iRODS paths. Paths can point to iRODS data 
+    objects or iRODS collections.
+
+    Input: irods.session object, list of iRODS path names
+    Output: cumulative sum of all file sizes
+    """
     irods_sizes = []
     for path_name in path_names:
         if session.data_objects.exists(path_name):
@@ -65,3 +97,8 @@ def get_irods_size(session: irods.session.iRODSSession, path_names: list) -> int
             coll = session.collections.get(path_name)
             irods_sizes.append(sum((sum((obj.size for obj in objs)) for _, _, objs in coll.walk())))
     return sum(irods_sizes)
+
+
+def annotate_data(session: irods.session.iRODSSession, irodspath: str, 
+                  localpath: str, serverip: str):
+
--- experiment/ssh.py
+++ experiment/ssh.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+
 RED = '\x1b[1;31m'
 DEFAULT = '\x1b[0m'
 YEL = '\x1b[1;33m'
@@ -7,13 +10,15 @@ serverip = "145.38.187.136"
 datauser = "cstaiger"
 destpath = "/home/cstaiger/mydata"
 sourcepath = "/Users/staig001/testdata"
+sudo = False
 
-import subprocess
-import sys
-#rsync --rsync-path="sudo rsync" <LOCALFILE> USER@SERVER2:/root
 
-def ssh_check_connection(datauser, serverip):
-    ssh = subprocess.run(["ssh", datauser+"@"+serverip, "uname -a"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def ssh_check_connection(datauser: str, serverip: str):
+    """
+    Check ssh datauser@serverip and execute uname -a.
+    """
+    ssh = subprocess.run(["ssh", datauser+"@"+serverip, "uname -a"],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if ssh.stderr:
         print(RED)
         print("Connection failed: ", datauser, serverip, DEFAULT)
@@ -22,15 +27,38 @@ def ssh_check_connection(datauser, serverip):
     else:
         print(BLUE, "Connected: ", datauser, serverip, DEFAULT)
 
-def rsync_local_to_remote(datauser, serverip, sourcepath, destpath):
-    print("Uploading data: %s --> %s@%s:%s" %(sourcepath, datauser, serverip, destpath))
-    res = subprocess.run(["rsync", "-rc", sourcepath, datauser+"@"+serverip+":"+destpath], 
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def rsync_local_to_remote(datauser: str, serverip: str, sudo: bool,
+                          sourcepath: str, destpath: str) -> str:
+    """
+    Transfers data from a local server to a remote linux server through rsync.
+    Assumes that an ssh keypair was installed for that user beforehand (local priv/pub key
+    and remote authorized_keys files are setup).
+
+    datauser: username on remote server
+    serverip: ip or fully qualified domain name of the remote server
+    sudo: if the destination on the remote server is protected to be written by the datauser,
+          sudo is needed to overrule that (not recommended)
+    sourcepath: local data path, can be file or folder
+    destpath: destination folder on remote server
+
+    Returns: '' (success), sourcepath (failure)
+    """
+
+    print("Uploading data: %s --> %s@%s:%s" % (sourcepath, datauser, serverip, destpath))
+    if sudo:
+        res = subprocess.run(['rsync', '--rsync-path="sudo rsync"', '-rc', sourcepath,
+                             datauser+"@"+serverip+':'+destpath],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+        res = subprocess.run(['rsync', '-rc',
+                             sourcepath, datauser+"@"+serverip+':'+destpath],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if res.stderr:
         print(RED, "rsync failed:", DEFAULT)
         print(res.stderr)
-        return(sourcepath)
+        return sourcepath
     elif res.stdout:
         print(YEL, "\t --> Data transfer complete", DEFAULT)
-        return
+        return ''
